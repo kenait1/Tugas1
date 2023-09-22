047f32276fe692b0cdc3d5fccdfcc2a3faeb8a52
kenait1 <tobby.a88@gmail.com>
2023-09-22 13:33:52 +0700

    tugas_1

---
+++ bin/tugas mc.dart
@@ -0,0 +1,50 @@
+class Mahasiswa {
+  String nama;
+  String nim;
+  String alamat;
+  String jenisKelamin;
+
+  // Konstruktor dengan parameter
+  Mahasiswa(String nama, String nim, String alamat, String jenisKelamin) {
+    this.nama = nama;
+    this.nim = nim;
+    this.alamat = alamat;
+    this.jenisKelamin = jenisKelamin;
+  }
+
+  // Metode untuk menampilkan informasi mahasiswa
+  void tampilkanInfo() {
+    print('Nama: $nama');
+    print('NIM: $nim');
+    print('Alamat: $alamat');
+    print('Jenis Kelamin: $jenisKelamin');
+    print('-----------------------------');
+  }
+}
+
+void main() {
+  // Membuat objek Mahasiswa
+  Mahasiswa mahasiswa1 = Mahasiswa('Simon', '12345', 'Jl. Merpati No. 3', 'Laki-Laki');
+  Mahasiswa mahasiswa2 = Mahasiswa('Anna', '67890', 'Jl. Pahlawan No. 4', 'Perempuan');
+  Mahasiswa mahasiswa3 = Mahasiswa('Jesi', '92038', 'Jl. Merdeka No. 7', 'Perempuan');
+  Mahasiswa mahasiswa4 = Mahasiswa('Bobi', '88937', 'Jl. Kuda No. 9', 'Laki-Laki');
+  Mahasiswa mahasiswa5 = Mahasiswa('Siti', '32230', 'Jl. Pagi No. 2', 'Perempuan');
+
+  // Menampilkan informasi mahasiswa
+  mahasiswa1.tampilkanInfo();
+  mahasiswa2.tampilkanInfo();
+  mahasiswa3.tampilkanInfo();
+  mahasiswa4.tampilkanInfo();
+  mahasiswa5.tampilkanInfo();
+}
+
+git config --global user.email "tobby.a88@gmail.com"
+git config --global user.name "kenait1" 
+git config --global user.password "anjing125anjing125"
+git init
+git add .
+git commit -m "tugas_1"
+git remote add origin https://github.com/kenait1/Tugas1.git
+git push -u origin master
+git push -u origin master
+
